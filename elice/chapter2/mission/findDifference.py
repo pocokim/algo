cac30b82787e43514b0af580c7405c039345e45c
shun <koownusmik@gmail.com>
2019-01-09 22:51:00 +0900

    미션 2-1 해결

--- elice/chapter2/mission/findDifference.py
+++ elice/chapter2/mission/findDifference.py
@@ -1,5 +1,15 @@
 def findDifference(str1, str2):
-    return ''
+    str1List = list(str1)
+    str2List = list(str2)
+    str1List.sort()
+    str2List.sort()
+
+    str1List.append('~')
+
+    for i in range(len(str2List)):
+        if str2List[i] != str1List[i]:
+            return str2List[i]
+
 
 def main():
     print(findDifference("apple", "azlppe"))
