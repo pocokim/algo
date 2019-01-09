4b484fed7b193fa87fc2e230b21affaa78fdf9e2
shun <koownusmik@gmail.com>
2019-01-09 21:57:07 +0900

    승태의 2중포문 풀이 update

--- elice/chapter1/findDuplicate.py
+++ elice/chapter1/findDuplicate.py
@@ -5,6 +5,12 @@ def findDuplicate(nums):
     #     if nums[i]==nums[i+1]:
     #         return nums[i]
 
+    # 승태 풀이 (이중포문 써서 단순비교)
+    # for i in range(len(nums) - 1):
+    # for j in range(i+1, len(nums)):
+    #     if nums[i] == nums[j]:
+    #         return nums[j]
+
     nums.sort()
     for i in range(1,len(nums)):
         if nums[i] == nums[i-1]:
