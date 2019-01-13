aa3e72eda898ddb90d1819a9ff514e8019ae21aa
shun <koownusmik@gmail.com>
2019-01-13 13:11:32 +0900

    문자열 -> 리스트 만드는법  modify

--- elice/chapter2/isAnagram.py
+++ elice/chapter2/isAnagram.py
@@ -1,23 +1,29 @@
 def isAnagram(str1, str2):
-    str1 =','.join(str1)
-    str1 = str1.split(',')
+    # str1List = list(str1)
+    # join , split 을 안쓰고도 list()로 문자열을 리스트로 만들 수 있음..
     
-    str2 = ','.join(str2)
-    str2 = str2.split(',')
 
-    for i in range(len(str1)):
-        str1[i] = ord(str1[i])
-    
-    for i in range(len(str2)):
-        str2[i] = ord(str2[i])
-    
-    str1.sort()
-    str2.sort()
 
-    if str1 == str2:
-        return True
-    else : 
-        return False
+    # str1 =','.join(str1)
+    # str1 = str1.split(',')
+    
+    # str2 = ','.join(str2)
+    # str2 = str2.split(',')
+    # print(str1)
+
+    # for i in range(len(str1)):
+    #     str1[i] = ord(str1[i])
+    
+    # for i in range(len(str2)):
+    #     str2[i] = ord(str2[i])
+    
+    # str1.sort()
+    # str2.sort()
+
+    # if str1 == str2:
+    #     return True
+    # else : 
+    #     return False
     
     # print(str1)
     # return ''
