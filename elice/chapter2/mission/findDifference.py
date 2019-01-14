bfef9b47eb00d7bea8014f6b676a9443fcde5b54
shun <koownusmik@gmail.com>
2019-01-14 16:24:56 +0900

    딕셔너리 사용 풀이2 update

--- elice/chapter2/mission/findDifference.py
+++ elice/chapter2/mission/findDifference.py
@@ -1,15 +1,24 @@
 def findDifference(str1, str2):
-    str1List = list(str1)
-    str2List = list(str2)
-    str1List.sort()
-    str2List.sort()
+    # 리스트 사용 
+    # str1List = list(str1)
+    # str2List = list(str2)
+    # str1List.sort()
+    # str2List.sort()
 
-    str1List.append('~')
+    # str1List.append('~')
 
-    for i in range(len(str2List)):
-        if str2List[i] != str1List[i]:
-            return str2List[i]
+    # for i in range(len(str2List)):
+    #     if str2List[i] != str1List[i]:
+    #         return str2List[i]
 
+    # 풀이 2 딕셔너리 사용     
+    dic = {}
+    for ch in str1:
+        dic[ch] = dic.get(ch,0) + 1
+
+    for ch in str2:
+        if ch not in dic:
+            return ch
 
 def main():
     print(findDifference("apple", "azlppe"))
