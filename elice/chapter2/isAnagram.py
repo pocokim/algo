bb6cab2a8c58d6166c5ca74c61b5ee4df7c30c68
shun <koownusmik@gmail.com>
2019-01-14 16:16:38 +0900

    리스트소팅, 딕셔너리로 풀이 update

--- elice/chapter2/isAnagram.py
+++ elice/chapter2/isAnagram.py
@@ -1,9 +1,37 @@
 def isAnagram(str1, str2):
-    # str1List = list(str1)
-    # join , split 을 안쓰고도 list()로 문자열을 리스트로 만들 수 있음..
-    
+
+    # # 풀이 1 : 리스트 소팅
+    #     str1List = list(str1)
+    #     str2List = list(str2)
+        
+    #     str1List.sort()
+    #     str2List.sort()
+        
+    #     return str1List == str2List
+        
+        dic1 = {}
+        for ch in str1:
+            dic1[ch] = int(dic1.get(ch,'0')) + 1 # 없으면 0을 있으면 갯수를 
+    # 풀이3  : 딕셔너리 2개 비교 
+    #     dic2 = {}
+    #     for ch in str2:
+    #         dic2[ch] = int(dic2.get(ch,'0')) +1 
+        
+    #     return dic1== dic2
+
+    # 풀이 2 : 딕셔너리 1개 만들고 문자열과 딕셔너리 비교 
+        for ch in str2:
+            if ch in dic1:
+                if dic1[ch] != 0:
+                    dic1[ch] -=1
+                    # print(dic1)
+                elif dic1[ch] == 0: return False
+            else : return False
+        return True 
 
 
+
+    # 풀이 4 안좋은풀이 
     # str1 =','.join(str1)
     # str1 = str1.split(',')
     
