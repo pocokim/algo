05129b5e2ddd58752e87cfe3e281c77b6f87e705
shun <koownusmik@gmail.com>
2019-01-17 19:17:29 +0900

    Create 1_factorial.py

---
+++ elice/chapter4/1_factorial.py
@@ -0,0 +1,12 @@
+def factorial(num):
+    if num == 1:
+        return 1
+    else : 
+        return num*factorial(num-1)
+     
+
+def main():
+    print(factorial(5)) # should return 120
+
+if __name__ == "__main__":
+    main()
