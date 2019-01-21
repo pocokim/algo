f7215e15e9edc862232de40fe856c73d60499fb8
shun <koownusmik@gmail.com>
2019-01-21 23:17:05 +0900

    list index out of range 인덱스가 다르면 반복문을 돌지않음.

--- elice/chapter2/mission/1_findDifference.py
+++ elice/chapter2/mission/1_findDifference.py
@@ -12,13 +12,38 @@ def findDifference(str1, str2):
     #         return str2List[i]
 
     # 풀이 2 딕셔너리 사용     
-    dic = {}
-    for ch in str1:
-        dic[ch] = dic.get(ch,0) + 1
+    # dic = {}
+    # for ch in str1:
+    #     dic[ch] = dic.get(ch,0) + 1
 
-    for ch in str2:
-        if ch not in dic:
-            return ch
+    # for ch in str2:
+    #     if ch not in dic:
+    #         return ch
+    
+    str1List = list(str1)
+    str2List = list(str2)
+    str1List.sort()
+    str2List.sort()
+    
+    print(str1List)
+    print(str2List)
+    
+#     print(str2List[1])
+#     print(str2List[2])
+
+# IndexError: list index out of range 배열 범위 문제가 계속 생김. 마지막 배열에 값을 넣어주어야할듯.. 
+#   elif str1List[i] != str2List[i]: 애초에 값에 i가 들어가지않음. 
+
+    
+    for i in range(len(str2List)):
+        if i == len(str1List) and str1List[i] == str2List[i]:
+            return str2List[-1]
+
+        elif str1List[i] != str2List[i]:
+            # print(str2List[i])
+            return str2List[i]
+                
+            
 
 def main():
     print(findDifference("apple", "azlppe"))
