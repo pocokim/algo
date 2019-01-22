3737b3f3c415ecd564959766a98a58994d1fe2fe
shun <koownusmik@gmail.com>
2019-01-22 09:58:22 +0900

    리스트 사용 풀이 업데이트

--- elice/chapter2/mission/1_findDifference.py
+++ elice/chapter2/mission/1_findDifference.py
@@ -1,4 +1,23 @@
 def findDifference(str1, str2):
+    dic= {}
+    for ch in str2:
+        dic[ch] = dic.get(ch,0) + 1
+    
+    
+    # for ch in str2:
+    #     if ch not in dic :
+    #         return ch 
+# 딕셔너리만 가지고 if문을 사용할 수 있을까? : z와 같은 다른 원소가 포함된 경우 사용가능 할 수 있지만 , 추가된 원소가 p일경우 잡을 수 없다고 생각함. 
+
+    for ch in str1:
+        if ch in dic :
+            dic[ch] -= 1
+    
+    for ch in str2:
+        if dic[ch] ==1:
+            return ch
+        
+
     # 리스트 사용 
     # str1List = list(str1)
     # str2List = list(str2)
@@ -20,13 +39,13 @@ def findDifference(str1, str2):
     #     if ch not in dic:
     #         return ch
     
-    str1List = list(str1)
-    str2List = list(str2)
-    str1List.sort()
-    str2List.sort()
+    # str1List = list(str1)
+    # str2List = list(str2)
+    # str1List.sort()
+    # str2List.sort()
     
-    print(str1List)
-    print(str2List)
+    # print(str1List)
+    # print(str2List)
     
 #     print(str2List[1])
 #     print(str2List[2])
@@ -35,18 +54,18 @@ def findDifference(str1, str2):
 #   elif str1List[i] != str2List[i]: 애초에 값에 i가 들어가지않음. 
 
     
-    for i in range(len(str2List)):
-        if i == len(str1List) and str1List[i] == str2List[i]:
-            return str2List[-1]
+    # for i in range(len(str2List)):
+    #     if i == len(str1List) and str1List[i] == str2List[i]:
+    #         return str2List[-1]
 
-        elif str1List[i] != str2List[i]:
-            # print(str2List[i])
-            return str2List[i]
+    #     elif str1List[i] != str2List[i]:
+    #         # print(str2List[i])
+    #         return str2List[i]
                 
             
 
 def main():
-    print(findDifference("apple", "azlppe"))
+    print(findDifference("apple", "aplppe"))
     
 
 if __name__ == "__main__":
