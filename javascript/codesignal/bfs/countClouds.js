57474b5b8277085e18760288f74385d9892601e1
shun <koownusmik@gmail.com>
2019-03-10 18:47:23 +0900

    최종 파일 수정

--- javascript/codesignal/bfs/countClouds.js
+++ javascript/codesignal/bfs/countClouds.js
@@ -7,23 +7,8 @@ function findCloud(skyMap,i,j){
     if( j>0 && skyMap[i][j-1] === '1' ) findCloud(skyMap,i,j-1);
     if( i<xlen-1 && skyMap[i+1][j] === '1' ) findCloud(skyMap,i+1,j);
     if( j<ylen-1 && skyMap[i][j+1] === '1' ) findCloud(skyMap,i,j+1);
-    
 }
-    //if( i>0 ){
-    //    if(skyMap[i-1][j] === '1') findCloud(skyMap,i-1,j);
-    //} 
-    // if( j>0 ){
-    //     if( skyMap[i][j-1] ==='1') findCloud(skyMap,i,j-1);
-    // } 
-    // if( i<xlen-1 ){
-    //     if( skyMap[i+1][j] ==='1') findCloud(skyMap,i+1,j);
-    // } 
-    // if( j<ylen-1 ){
-    //     if( skyMap[i][j+1] ==='1') findCloud(skyMap,i,j+1);
-    // 
-    // javascript 에서는 skyMap[-1]이런 값을 읽어올 수 없으므로
-    // 처음 if문에서 i,j의 범위값을 조절 
-    
+
 
 
 
@@ -32,7 +17,6 @@ function countClouds(skyMap) {
     for(let i =0; i< skyMap.length; i++){
         for(let j=0; j<skyMap[0].length; j++){
             if(skyMap[i][j] === '1') {
-                console.log(i,j)
                 findCloud(skyMap,i,j)
                 ans += 1;
             }
