2a05d9a1f4d48ecff6b95d6ba27ff7821188a72b
shun <koownusmik@gmail.com>
2019-03-10 18:25:22 +0900

    해결한 풀이
    xlen이랑 ylen을 바꿔서 넣어서 못풀었었음.
    
    그런데  if( i>0 && skyMap[i-1][j] === '1')와
     //if( i>0 ){
        //    if(skyMap[i-1][j] === '1') findCloud(skyMap,i-1,j);
        //}
    
    이 같다고 말할 수 있는지?

--- javascript/codesignal/bfs/countClouds.js
+++ javascript/codesignal/bfs/countClouds.js
@@ -1,21 +1,38 @@
 
 function findCloud(skyMap,i,j){
-    const len = skyMap[0].length; 
+    const ylen = skyMap[0].length;
+    const xlen = skyMap.length; 
     skyMap[i][j] = '0'
-    if( i>0 && skyMap[i-1][j] === '1')  findCloud(skyMap,i-1,j);
-    if( j>0 && skyMap[i][j-1] ==='1') findCloud(skyMap,i,j-1);
-    if( i<len && skyMap[i+1][j] ==='1') findCloud(skyMap,i+1,j);
-    if( j<len && skyMap[i][j+1] ==='1') findCloud(skyMap,i,j+1);
-    // javascript 에서는 skyMap[-1]이런 값을 읽어올 수 없음.
+    if( i>0 && skyMap[i-1][j] === '1' ) findCloud(skyMap,i-1,j);
+    if( j>0 && skyMap[i][j-1] === '1' ) findCloud(skyMap,i,j-1);
+    if( i<xlen-1 && skyMap[i+1][j] === '1' ) findCloud(skyMap,i+1,j);
+    if( j<ylen-1 && skyMap[i][j+1] === '1' ) findCloud(skyMap,i,j+1);
+    
+}
+    //if( i>0 ){
+    //    if(skyMap[i-1][j] === '1') findCloud(skyMap,i-1,j);
+    //} 
+    // if( j>0 ){
+    //     if( skyMap[i][j-1] ==='1') findCloud(skyMap,i,j-1);
+    // } 
+    // if( i<xlen-1 ){
+    //     if( skyMap[i+1][j] ==='1') findCloud(skyMap,i+1,j);
+    // } 
+    // if( j<ylen-1 ){
+    //     if( skyMap[i][j+1] ==='1') findCloud(skyMap,i,j+1);
+    // 
+    // javascript 에서는 skyMap[-1]이런 값을 읽어올 수 없으므로
+    // 처음 if문에서 i,j의 범위값을 조절 
     
 
-}
+
 
 function countClouds(skyMap) {
     let ans = 0;
     for(let i =0; i< skyMap.length; i++){
         for(let j=0; j<skyMap[0].length; j++){
             if(skyMap[i][j] === '1') {
+                console.log(i,j)
                 findCloud(skyMap,i,j)
                 ans += 1;
             }
